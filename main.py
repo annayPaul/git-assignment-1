5e0b572d7aa2c28b7ccfb8d363bd8116f3e036a6
Annay Paul <annay.paul@brainstation-23.com>
2024-04-25 11:44:07 +0600

    Added a program for calculating the nth fibonacci number

--- main.py
+++ main.py
@@ -6,4 +6,10 @@ def factorial(number):
     else:
         return number * factorial(number - 1)
 
+# number is the offset, starts at 0, eg: 0th fibonacci is 1, 1st fibonacci is 1, etc
+def fibonacci(number):
+    if number <= 1:
+        return number
+    return fibonacci(number-1) + fibonacci(number)
+
 print(factorial(n))
